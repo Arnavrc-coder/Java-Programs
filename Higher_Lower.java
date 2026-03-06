816e188a6019157caf5ce0c896e6e14e950b15f4
Arnav Raj Choudhary <your_email@gmail.com>
2026-03-06 20:27:15 +0530

    Added Higher Lower Java game

---
+++ Higher_Lower.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Higher_Lower {
+	public static void main(String args[]) {
+		int ran1, ran2;
+		Scanner in = new Scanner(System.in);
+		ran1 = (int) Math.round(Math.random() * 100 +1);
+		ran2 = (int) Math.round(Math.random() * 100 +1);
+		System.out.println("Hello Player! You are playing a game.");
+		System.out.println("The computer has generated a random number.");
+		System.out.println("You have to tell that the next randomly generated no. will be greater or lesser than this no.");
+		System.out.println("The current random no. is : " + ran1);
+		System.out.println("Enter H if you think the next no. will be higher or enter L if you think the next no. will be lower");
+		String guess = in.nextLine().toUpperCase();
+		if(guess.equals("H")) {
+			if(ran2 > ran1)
+				System.out.println("You win!");
+			else
+				System.out.println("You lost!");
+		}
+		else {
+			if(ran2 < ran1)
+				System.out.println("You win!");
+			else
+				System.out.println("You lost!");
+		}
+	}
+}
